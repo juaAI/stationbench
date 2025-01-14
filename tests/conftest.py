5b7e97c255fe01eb91f9a0eebfcdad7c29d0b63c
leoniewgnr <42536262+leoniewgnr@users.noreply.github.com>
2025-01-14 09:40:29 +0100

    Tests (#9)
    * added tests
    
    * add tests as github action
    
    * only run tests on 3.11
    
    * updated toml file
    
    * adapted gh action
    
    * add dev flag to action

---
+++ tests/conftest.py
@@ -0,0 +1,54 @@
+import pytest
+import xarray as xr
+import numpy as np
+import pandas as pd
+
+
+@pytest.fixture
+def sample_forecast_dataset():
+    # Create a small synthetic forecast dataset
+    times = pd.date_range("2024-01-01", "2024-01-02", freq="D")
+    lats = np.linspace(45, 50, 3)
+    lons = np.linspace(-5, 5, 4)
+    prediction_timedeltas = np.array([0, 6, 12, 24], dtype="timedelta64[h]")
+
+    ds = xr.Dataset(
+        data_vars={
+            "10m_wind_speed": (
+                ["init_time", "lead_time", "latitude", "longitude"],
+                np.random.rand(
+                    len(times), len(prediction_timedeltas), len(lats), len(lons)
+                ),
+            )
+        },
+        coords={
+            "time": times,
+            "prediction_timedelta": prediction_timedeltas,
+            "latitude": lats,
+            "longitude": lons,
+        },
+    )
+    return ds
+
+
+@pytest.fixture
+def sample_ground_truth_dataset():
+    # Create a small synthetic ground truth dataset
+    times = pd.date_range("2024-01-01", "2024-01-03", freq="h")
+    stations = range(3)
+
+    ds = xr.Dataset(
+        data_vars={
+            "10m_wind_speed": (
+                ["time", "station_id"],
+                np.random.rand(len(times), len(stations)),
+            ),
+            "latitude": ("station_id", [46, 47, 48]),
+            "longitude": ("station_id", [-2, 0, 2]),
+        },
+        coords={
+            "time": times,
+            "station_id": stations,
+        },
+    )
+    return ds
